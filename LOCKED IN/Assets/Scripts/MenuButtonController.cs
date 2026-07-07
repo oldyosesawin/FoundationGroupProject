0716dc35c3404ce3502d48e4e480a543fb2c83ab
oldyosesawin <bendymurdersans@gmail.com>
2026-07-07 15:16:27 +0700

    Menu,Setting and Credit
    Menu done(need BG and BG music)
    Credit done(Need BG and BG music)

---
+++ LOCKED IN/Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Setting()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturntoMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
