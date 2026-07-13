b4174504d95eb423feb272e064ad87561f79892f
LEE-MS23 <pasinp2550@gmail.com>
2026-07-13 18:09:14 +0700

    battle 3 update

---
+++ LOCKED IN/Assets/Seph(Artist)/SephToBattleAnimation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SephToBattleAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public string triggerName = "NextAnim";
+    public float delayInSeconds = 2.0f;
+
+    public void OnButtonClick()
+    {
+        StartCoroutine(PlayAnimationWithDelay());
+    }
+
+
+    private IEnumerator PlayAnimationWithDelay()
+    {
+
+        yield return new WaitForSeconds(delayInSeconds);
+
+
+        animator.SetTrigger(triggerName);
+    }
+}
